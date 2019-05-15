565a192baff79e094414e02f5118c629f15c3ff4
willow <shaneweth77@gmail.com>
2019-05-15 11:58:03 -0400

    changes to main.handlebars and index.handlebars

--- models/wheelbarrow.js
+++ models/wheelbarrow.js
@@ -1,18 +1,30 @@
-const orm = require("../config/orm");
 
-// call the ORM functions in order to move the rocks
+// Import the ORM to implement functions that will interact with the database
+var orm = require('../config/orm.js');
 
-var wheelbarrow = {
-    selectAll: function (cb) {
-        orm.selectAll("rocks", function (res) {
-            cb(res);
-        });
-    },
+// Create the rock object
+var rock = {
+  // Select all rock table entries
+  selectAll: function(cb) {
+    orm.selectAll('rocks', function(res) {
+      cb(res);
+    });
+  },
 
-    insertOne: function (col, val, cb) {
-        orm.insertOne("rocks", col, val, function(res){
-            cb(res);
-        })
-    }
+  // The variables cols and vals are arrays
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('rocks', cols, vals, function(res) {
+      cb(res);
+    });
+  },
 
-}
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('rocks', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgerController.js).
+module.exports = rock;
