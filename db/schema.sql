e139119049f0e3513f148fb1713a51c0164a7c13
willow <shaneweth77@gmail.com>
2019-05-16 16:37:26 -0400

    adjustments to layout and responsive.

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE wheelbarrow_db;
 
 CREATE TABLE rocks (
     id INT NOT NULL AUTO_INCREMENT,
-    rock_name VARCHAR(10) NULL,
+    rock_name VARCHAR(255) NULL,
     in_bucket BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
