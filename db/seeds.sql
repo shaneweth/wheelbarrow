2d44c7daca5e8583f5fade43142b9dbdd64582f6
willow <shaneweth77@gmail.com>
2019-05-16 15:14:16 -0400

    final commit - style is finished-ish

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO rocks (rock_name) VALUES (granite);
-INSERT INTO rocks (rock_name) VALUES (quartz);
-INSERT INTO rocks (rock_name) VALUES (opal);
-INSERT INTO rocks (rock_name) VALUES (garnet);
-INSERT INTO rocks (rock_name) VALUES (soapstone);
+INSERT INTO rocks (rock_name) VALUES ("granite");
+INSERT INTO rocks (rock_name) VALUES ("quartz");
+INSERT INTO rocks (rock_name) VALUES ("opal");
+INSERT INTO rocks (rock_name) VALUES ("garnet");
+INSERT INTO rocks (rock_name) VALUES ("soapstone");
