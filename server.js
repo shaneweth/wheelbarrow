e29472a909a5565263847c34509df8677eea0c98
willow <shaneweth77@gmail.com>
2019-05-16 13:22:06 -0400

    resolved css link issue. added some style changes

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var path = require('path');
 
-var port = process.env.PORT || 3000;
-
 var app = express();
 
-// Serve static content for the app from the 'public' directory
-app.use(express.static(path.join(__dirname, './public')));
+var port = process.env.PORT || 3000;
 
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
